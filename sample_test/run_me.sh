2f12d22e04ded1734b859f794d5549d329b21e0a
yutingsdu <yutingsdu@163.com>
2022-04-20 17:49:10 +0800

    Add files via upload

--- sample_test/run_me.sh
+++ sample_test/run_me.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+
+## Please first dowload the demo data from https://sourceforge.net/projects/transassembly/files/Gingko/DemoData/
+
+# !!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!!!!!!
+
+#export LD_LIBRARY_PATH=/home/yuting/yuting/Software/local/boost/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/yuting/local/boost/lib:$LD_LIBRARY_PATH
+
+../TransMeta -B bamlist -s first -o transmeta_oudir -p 2
