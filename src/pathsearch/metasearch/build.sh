580d3ba7555905b4ad24c11af4f0c012d4ec416a
yutingsdu <yutingsdu@163.com>
2022-06-06 16:51:20 +0800

    Update build.sh

--- src/pathsearch/metasearch/build.sh
+++ src/pathsearch/metasearch/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ transmeta_path_search.cpp junction_graph.cc	QuadProg++.cc  simplify_graph.cc -o path_search -I /home/yuting/yuting/Software/local/boost/include/ -L /home/yuting/yuting/Software/local/boost/lib/
+g++ transmeta_path_search.cpp junction_graph.cc	QuadProg++.cc  simplify_graph.cc -o path_search -I /home/yuting/Software/local/bamtools_1_60/include/ -L /home/yuting/Software/local/bamtools_1_60/lib/
